62f6211556846bec8f5c848df430f1ff5b54a5ed
day <parknalee@day-nars.local>
2024-10-15 23:05:50 +0900

    remove note

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 	kotlin("plugin.jpa") version "1.9.25"
 	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
-//	id("org.flywaydb.flyway") version "10.18.0"
 }
 
 group = "dev.nars"
