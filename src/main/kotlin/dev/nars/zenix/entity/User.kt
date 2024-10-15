926c3dfc171476ba3963c9c1f29efec7a1d2eb85
day <parknalee@day-nars.local>
2024-10-16 00:40:06 +0900

    add business info

--- src/main/kotlin/dev/nars/zenix/entity/User.kt
+++ src/main/kotlin/dev/nars/zenix/entity/User.kt
@@ -1,5 +1,6 @@
 package dev.nars.zenix.entity
 
+import dev.nars.zenix.entity.embeddable.BusinessInfo
 import dev.nars.zenix.enumeration.UserStatus
 import jakarta.persistence.*
 
@@ -8,6 +9,9 @@ class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long,
+
+    @Column(unique = true, length = 32)
+    var nickname: String,
 
     @Column(unique = true, length = 32)
     var username: String,
@@ -20,6 +24,22 @@ class User(
 
     @Column(updatable = false)
     val isBot: Boolean = false,
+
+    var isStar: Boolean = false,
+
+    var profileUrl: String? = null,
+
+    var postCnt: Int = 0,
+
+    var followerCnt: Int = 0,
+
+    var followingCnt: Int = 0,
+
+    @Column(length = 64)
+    var description: String? = null,
+
+    @Embedded
+    var businessInfo: BusinessInfo? = null,
 
     @Enumerated(EnumType.STRING)
     @Column(length = 20)
---
+++ src/main/kotlin/dev/nars/zenix/enumeration/BusinessType.kt
@@ -0,0 +1,20 @@
+package dev.nars.zenix.enumeration
+
+enum class BusinessType(
+    val title: String
+) {
+    ARTS_AND_ENTERTAINMENT("예체능"),
+    ATHLETE("운동선수"),
+    CREATOR("크리에이터"),
+    EDUCATION("교육"),
+    FASHION("패션"),
+    FOOTWEAR("신발"),
+    HEALTH_AND_BEAUTY("건강 및 뷰티"),
+    INFLUENCER("인플루엔서"),
+    PHOTOGRAPHER("사진작가"),
+    PRODUCT_AND_SERVICE("제품 및 서비스"),
+    SPORTS_GOODS("운동 용품"),
+    SPORTS_MEDICINE("스포츠 의학"),
+    SPORTS_TEAM("스포츠팀"),
+    TUTOR("운동 강사"),
+}
